df888b74cf2e0f0199ef82bc08c238016052897d
Sean Cleveland <sean.b.cleveland@gmail.com>
2012-09-16 18:50:43 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nadv::Application.config.secret_token = '74e50023dc976d6c5769867f2041df22ce20fad797822530de681946b58048b8aca1db22cc0b28ee0f0a25c4905443b2a5fd9b81915be91e3856428cfc19caba'
