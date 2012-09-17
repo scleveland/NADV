a542564aab933d71923dda2014ac785cd7a982e2
Sean Cleveland <sean.b.cleveland@gmail.com>
2012-09-16 22:13:39 -0600

    added logic to only show translation after seeing a start codon

--- app/controllers/sequence_controller.rb
+++ app/controllers/sequence_controller.rb
@@ -11,10 +11,27 @@ class SequenceController < ApplicationController
     window_size = 3
     step_size   = 3
 
+    start_flag=false
+    stop_flag = true
     # this will print all the codons as "aug", "gca", "ccg", "ucc", "aga"
     @seq.window_search(window_size, step_size) do |subseq|
-      @aaseq_w_space = @aaseq_w_space + subseq.translate + "__"
-      @aaseq = @aaseq + subseq.translate
+      if subseq.translate == "M"
+        start_flag = true
+      end
+      if subseq.translate == "*"
+        if start_flag
+          start_flag=false
+           @aaseq_w_space = @aaseq_w_space + subseq.translate + "__"
+           @aaseq = @aaseq + subseq.translate
+        else
+          @aaseq_w_space = @aaseq_w_space + "___"
+        end
+      elsif start_flag # && stop_flag
+        @aaseq_w_space = @aaseq_w_space + subseq.translate + "__"
+        @aaseq = @aaseq + subseq.translate
+      else
+        @aaseq_w_space = @aaseq_w_space + "___"
+      end
     end
 
   end
@@ -26,15 +43,35 @@ class SequenceController < ApplicationController
     window_size = 3
     step_size   = 3
     # this will print all the codons as "aug", "gca", "ccg", "ucc", "aga"
+    start_flag=false
+    stop_flag = true
     seq.window_search(window_size, step_size) do |subseq|
-      aaseq = aaseq + subseq.translate + "\s\s"
+      #aaseq = aaseq + subseq.translate + "\s\s"
+      
+      if subseq.translate == "M"
+        start_flag = true
+      end
+      if subseq.translate == "*"
+        if start_flag
+          start_flag=false
+          aaseq = aaseq + subseq.translate + "\s\s"
+        else
+          aaseq = aaseq + "\s\s\s"
+        end
+      elsif start_flag # && stop_flag
+        aaseq = aaseq + subseq.translate + "\s\s"
+      else
+        aaseq = aaseq + "\s\s\s"
+      end
+      
     end
-    rows = (seq.length/30).abs 
+    nuc_num = 60
+    rows = (seq.length/nuc_num).abs 
     i = 0  
     # Create a new file and write to it   
     while i < rows  do 
-      out_string = out_string + ((10*i)+1).to_s + "\t" + aaseq[(30*i)..(30*i+30-1)] + "\n"
-      out_string = out_string + ((30*i)+1).to_s + "\t" + seq.upcase[(30*i)..(30*i+30-1)] + "\n"
+      out_string = out_string + ((nuc_num/3*i)+1).to_s + "\t" + aaseq[(nuc_num*i)..(nuc_num*i+nuc_num-1)] + "\n"
+      out_string = out_string + ((nuc_num*i)+1).to_s + "\t" + seq.upcase[(nuc_num*i)..(nuc_num*i+nuc_num-1)] + "\n"
       i +=1  
     end 
     #render :file=> out_string
